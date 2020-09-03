22b08e169c87f5a8acacf6bee9181a8c5b10faea
vatsal <vatsal@wondereur.com>
2020-09-03 01:24:33 -0400

    created new web pages to link to the original page for NER and text summarization

--- app.py
+++ app.py
@@ -1,110 +1,61 @@
-from flask import Flask, request, jsonify, render_template
-from nltk.corpus import stopwords
-from nltk.cluster.util import cosine_distance
 import numpy as np
 import networkx as nx
+import text_summarizer as ts
+
+from flask import Flask, request, jsonify, render_template
+from Levenshtein import jaro
+from nltk.corpus import stopwords
+from nltk.cluster.util import cosine_distance
 
 # refrence
 # https://towardsdatascience.com/understand-text-summarization-and-create-your-own-summarizer-in-python-b26a9f09fc70
 
 app = Flask(__name__)
 
-def read_article(data):
-    article = data.split('. ')
-    sentences = []
-    for sentence in article:
-        sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
-    sentences.pop()
-    return sentences
-
-def sentence_similarity(sent1, sent2, stopwords=None):
-    if stopwords is None:
-        stopwords = []
- 
-    sent1 = [w.lower() for w in sent1]
-    sent2 = [w.lower() for w in sent2]
- 
-    all_words = list(set(sent1 + sent2))
- 
-    vector1 = [0] * len(all_words)
-    vector2 = [0] * len(all_words)
- 
-    # build the vector for the first sentence
-    for w in sent1:
-        if w in stopwords:
-            continue
-        vector1[all_words.index(w)] += 1
- 
-    # build the vector for the second sentence
-    for w in sent2:
-        if w in stopwords:
-            continue
-        vector2[all_words.index(w)] += 1
- 
-    return 1 - cosine_distance(vector1, vector2)
- 
-def build_similarity_matrix(sentences, stop_words):
-    # Create an empty similarity matrix
-    similarity_matrix = np.zeros((len(sentences), len(sentences)))
- 
-    for idx1 in range(len(sentences)):
-        for idx2 in range(len(sentences)):
-            if idx1 == idx2: #ignore if both are same sentences
-                continue 
-            similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
-
-    return similarity_matrix
-
-def generate_summary(data, top_n=5):
-    stop_words = stopwords.words('english')
-    summarize_text = []
-
-    # Step 1 - Read text anc split it
-    sentences =  read_article(data)
-
-    # Step 2 - Generate Similary Martix across sentences
-    sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
-
-    # Step 3 - Rank sentences in similarity martix
-    sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
-    scores = nx.pagerank(sentence_similarity_graph)
-
-    # Step 4 - Sort the rank and pick top sentences
-    ranked_sentence = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)    
-#     print("Indexes of top ranked_sentence order are ", ranked_sentence)    
-
-    for i in range(top_n):
-        summarize_text.append(" ".join(ranked_sentence[i][1]))
-
-    # Step 5 - Offcourse, output the summarize texr
-    return ". ".join(summarize_text)
-
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-@app.route('/', methods = ['POST'])
+@app.route('/summarizer', methods = ['GET', 'POST'])
+def summarizer():
+    return render_template('summarizer.html')
+
+@app.route('/predict', methods = ['GET', 'POST'])
 def predict():
     '''
     This will output the summary of a text given the input
     '''
     body = str(request.form['Body'])
-    result = generate_summary(data = body, top_n = 5)
-    return render_template('index.html', prediction_text='{}'.format(result))
+    sentence_count = int(request.form['sentence_count'])
+    result = generate_summary(data = body, top_n = sentence_count)
+    return render_template('summarizer.html', prediction_text='{}'.format(result))
+
+@app.route('/ner', methods = ['GET', 'POST'])
+def ner():
+    return render_template('ner.html')
+
+@app.route('/predict_ner', methods = ['GET', 'POST'])
+def predict_ner():
+    '''
+    This will return the similarity between two strings
+    '''
+
+    str1 = str(request.form['string1'])
+    str2 = str(request.form['string2'])
+    result = jaro(str1, str2)
+    return render_template('ner.html', prediction_text = '{}'.format(result))
 
 # @app.route('/predict_api',methods=['POST'])
 # def predict_api():
 #     '''
 #     For direct API calls through request
 #     '''
-
+#
 #     data = request.get_json(force=True)
 #     output = model(data, min_length = 60)
-    
+#
 #     return jsonify(output)
 
 if __name__ == "__main__":
-    app.run(debug=True)
 
-    
-    
+    app.run(debug=True)
